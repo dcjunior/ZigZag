73916e7bf918afd3ef69d0f6a1d63103c6abcd9f
unknown <zubinchoudhary@gmail.com>
2018-03-03 14:37:24 +0530

    ZigZag

---
+++ ZigZagv1/Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 10;
+
+	private Vector3 dir;
+
+	private static Movement inst;
+
+	public static Movement Inst {
+		get {
+			if (inst == null) {
+				inst = GameObject.FindObjectOfType<Movement> ();
+			}
+			return inst;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		dir = Vector3.zero;
+		
+	}
+
+	public void Chck(){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			if (dir == Vector3.forward)
+				dir = Vector3.left;
+			else
+				dir = Vector3.forward;
+		}
+
+		float amountToMove = speed * Time.deltaTime;
+		transform.Translate (dir * amountToMove);
+	}
+}
---
+++ ZigZagv1/Assets/Scripts/Spawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject CurrentTile;
+	public GameObject[] tilePrefab;
+	private static Spawn instance;
+
+	public static Spawn Instance {
+		get {
+			if (instance == null) {
+				instance = GameObject.FindObjectOfType<Spawn> ();
+			}
+			return instance;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		for(int i =0;i<5;i++)
+		SpawnTiles ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SpawnTiles(){
+		int ran = Random.Range (0, 2);
+		CurrentTile = (GameObject)Instantiate (tilePrefab[ran], CurrentTile.transform.GetChild (0).transform.GetChild(ran).position,Quaternion.identity);
+
+	
+	}
+
+}
